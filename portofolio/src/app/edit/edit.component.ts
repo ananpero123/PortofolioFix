ee30d0cbe3a5888327481d4fedc6c51fba6d7566
ohsyith <mnasyith2006@gmail.com>
2023-08-09 14:50:22 +0700

    ttt

--- portofolio/src/app/edit/edit.component.ts
+++ portofolio/src/app/edit/edit.component.ts
@@ -1,87 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ApinampilService } from '../Services/apinampil.service';
-import { Porto } from '../Model/Porto';
-import { Observable, Subscriber } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Firestore, doc, DocumentSnapshot, DocumentData, getDoc, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css'],
+  styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-  portofolioId: number | null = null;
-  edit: Porto | null = null;
+  title = 'Project_Portofolio';
+  userId: string | null = null;
+  userData: DocumentData  = {};
 
-  constructor(
-    private route: ActivatedRoute,
-    private apiService: ApinampilService,
-    private router: Router
-  ) {}
+  constructor(private route: ActivatedRoute, private firestore: Firestore) {}
 
-  ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.portofolioId = +params['id'];
-      if (this.portofolioId !== null) {
-        this.loadPortofolio(this.portofolioId);
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.userId = params.get('id');
+      if (this.userId) {
+        this.getData();
+        console.log(this.userId);
+        
       }
     });
   }
 
-  loadPortofolio(id: number): void {
-    this.apiService.ambilById(id).subscribe((portofolio: Porto) => {
-      if (portofolio) {
-        this.edit = portofolio;
-      }
-    });
-  }
-
-  onSubmit(): void {
-    if (this.portofolioId !== null && this.edit !== null) {
-
-      this.apiService
-        .updatePortofolio(this.portofolioId, this.edit)
-        .subscribe((response) => {
-          this.router.navigate(['/portofolio']);
-        });
+  async getData() {
+    const docRef = doc(this.firestore, `portofolio/${this.userId}`);
+    const docSnap: DocumentSnapshot<DocumentData> = await getDoc(docRef);
+    if (docSnap.exists()) {
+      this.userData = docSnap.data();
+    } else {
+      console.log('Document not found!');
     }
   }
 
-  myImage!: Observable<any>;
-
-  base64code!: any;
-
-  onChange = ($event : Event)  => {
-    const target = $event.target as HTMLInputElement;
-    const file: File = (target.files as FileList)[0];
-    console.log(file);
-
-    this.convertToBase64(file)
-  }
-
-  convertToBase64(file: File){
-    const observable = new Observable((subscriber: Subscriber<any>) => {
-      this.readFile(file, subscriber)
-    });
-
-    observable.subscribe((d) => {
-      if(this.edit != null){
-        this.edit.foto = d;
-        this.myImage = d;
-      }
-    });
-  }
-
-  readFile(file: File, subscriber: Subscriber<any>) {
-    const filereader = new FileReader();
-    filereader.readAsDataURL(file);
-    filereader.onload = () => {
-      subscriber.next(filereader.result);
-      subscriber.complete();
-    }
-    filereader.onerror = () => {
-      subscriber.error();
-      subscriber.complete();
+  async saveData() {
+    if (this.userData && this.userId) {
+      const docRef = doc(this.firestore, 'portofolio', this.userId);
+      await updateDoc(docRef, this.userData);
+      console.log('Data updated successfully!');
     }
   }
 }
+
